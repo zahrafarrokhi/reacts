89c4b89031a346882614564d873fbd034f49c947
yasi <zahrafarrokhi2017@gmail.com>
2022-07-15 22:38:22 +0430

    fix axios

--- website/frontend/frontend/lib/axios.js
+++ website/frontend/frontend/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-// when accessToken expires, use refreshToken to get new accessToken
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
-// Create axios instance.
 const axiosInstance = axios.create({
-  // baseURL: process.env.BACKEND_BASE_URL,  #becuse for confing on next.config.js and traefik
   withCredentials: true,
   headers: {
     common: {
@@ -14,34 +11,23 @@ const axiosInstance = axios.create({
 });
 
 export const setupInterceptors = (store) => {
+  createAuthRefreshInterceptor(axiosInstance, (failedRequest) => axiosInstance
+    .post('/api/auth/refresh/', {
+      user_id: store.getState().authReducer?.username,
+      refresh: store.getState().authReducer?.refreshToken,
+    })
+    .then((resp) => {
+      const { access_tok: accessToken } = resp.data;
+      const bearer = `${
+        process.env.JWT_AUTH_HEADER ?? 'Bearer'
+      } ${accessToken}`;
+      console.log(accessToken);
+      axiosInstance.defaults.headers.common.Authorization = bearer;
 
-
-  createAuthRefreshInterceptor(axiosInstance, (failedRequest) => {
-    return axiosInstance
-    // TokenRefreshSerializer ,220,221
-      .post('/api/auth/refresh/', {
-        user_id: store.getState().authReducer?.username,
-        refresh: store.getState().authReducer?.refreshToken,
-      })
-      .then((resp) => {
-        // TokenRefreshSerializer ,237
-        const { access_tok: accessToken } = resp.data;
-        const bearer = `${
-          process.env.JWT_AUTH_HEADER ?? 'Bearer'
-        } ${accessToken}`;
-        console.log(accessToken);
-        axiosInstance.defaults.headers.Authorization = bearer;
-
-       
-        failedRequest.response.config.headers.Authorization = bearer;
-        return Promise.resolve();
-      })
-    }
-  );
+      failedRequest.response.config.headers.Authorization = bearer;
+      return Promise.resolve();
+    }), { statusCodes: [401, 403] });
 };
-
-
-
 
 // Create axios interceptor
 export default axiosInstance;
