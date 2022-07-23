5afa3e83675d6815f018627114dda842a0bf80c0
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-07-23 12:33:04 +0430

    add DOCTORSDEGREES

--- website/frontend/frontend/lib/utils.js
+++ website/frontend/frontend/lib/utils.js
@@ -7,3 +7,10 @@ export const persianToEnglishDigits = (digit) => String(digit)
   .replace(/[٠-٩]/g, (d) => '٠١٢٣٤٥٦٧٨٩'.indexOf(d))
   .replace(/[۰-۹]/g, (d) => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d));
 
+
+  export const DOCTORSDEGREES = {
+    general: 'عمومی',
+    specialist: 'تخصص',
+    expert: 'فوق تخصص',
+    fellowship: 'فلوشیپ',
+  };
